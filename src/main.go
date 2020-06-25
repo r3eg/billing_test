d0991bef9575c21c52e3116c6dfc796f93cc178d
Vladimir <ra3eg@mail.ru>
2020-06-25 21:34:07 +0300

    Added generate msg from db

--- src/main.go
+++ src/main.go
@@ -9,18 +9,39 @@ import (
 	"os"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 )
 
 // Message is a base struct for messages
 type Message struct {
-	ID       string `json:"id"`
-	ClientID string `json:"clientId"`
-	Text     string `json:"text"`
-	Operator string `json:"operator"`
+	ID       string `db:"id" json:"id"`
+	ClientID string `db:"clientId" json:"clientId"`
+	Text     string `db:"text" json:"text"`
+	Operator string `db:"operator" json:"operator"`
 }
 
+var db *sqlx.DB
+
 func main() {
+	initAMQP()
+	initDB()
+
+	gin.SetMode("release")
+	r := gin.New()
+	r.GET("/handle/:count", func(c *gin.Context) {
+		count := c.Param("count")
+		generateMessages(count)
+	})
+	host := ":14500"
+	logrus.Info("running http server on host: ", host)
+	r.Run(host)
+}
+
+func initAMQP() {
 	amqpHost := os.Getenv("AMQP_HOST")
 	if amqpHost == "" {
 		amqpHost = "localhost"
@@ -28,17 +49,52 @@ func main() {
 	isInit := make(chan string)
 	go initPublisher("amqp://guest:guest@"+amqpHost+":5672", "test.billing", "fanout", isInit)
 	<-isInit
-	countMessages := 100000
+}
+
+func initDB() {
+	var err error
+	connected := false
+	defaultURI := "user=postgres password=VjMaexz$rF dbname=billing sslmode=disable"
+	envURI := os.Getenv("SQL_URI")
+	if envURI == "" {
+		envURI = defaultURI
+	}
+	for !connected {
+		db, err = sqlx.Connect("postgres", envURI)
+		if err != nil {
+			logrus.Error("failed connect to database:", envURI, " ", err, " try reconnect after 20 seconds")
+			time.Sleep(20 * time.Second)
+			continue
+		} else {
+			connected = true
+		}
+	}
+	logrus.Info("success connect to database: ", envURI)
+}
+
+func generateMessages(count string) {
+	data := []Message{}
+	minLen := 50
+	maxLen := 1000
+
+	err := db.Select(&data, `SELECT id as "clientId", tariff as "operator" FROM client LIMIT `+count)
+	if err != nil {
+		logrus.Error("err get client data: ", err)
+		return
+	}
 	//prepare messages
+	logrus.Info("prepare messages: ", count)
 	now := time.Now()
-	for i := 0; i < countMessages; i++ {
+	for i := 0; i < len(data); i++ {
+		msg := data[i]
 		go func() {
-			msg := Message{
-				ID:       randomString(32, false),
-				ClientID: randomString(32, false),
-				Text:     randomString(170, false),
-				Operator: randomString(32, false),
-			}
+			// prepare message with random length
+			msg.ID = NewId()
+			rand.Seed(time.Now().UnixNano())
+			msgLen := rand.Intn(maxLen-minLen+1) + minLen
+			msg.Text = randomString(msgLen, false)
+
+			// send message to amqp exchange
 			data, err := json.Marshal(msg)
 			if err != nil {
 				logrus.Error("[main] ", "Error json marshal: ", err)
@@ -49,10 +105,12 @@ func main() {
 	}
 	after := time.Since(now).Seconds()
 	logrus.Info("seconds: ", after)
-	for {
+}
 
-	}
-
+// NewId returns new UUID
+func NewId() (res string) {
+	uuidVal := uuid.New().String()
+	return uuidVal
 }
 
 func sendRequest(reqURL string, method string, data []byte) ([]byte, error) {
